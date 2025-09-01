ac519f6933241060361ef65edc5f8c2dc72de565
jass2k5 <jsssndl@gmail.com>
2025-09-02 02:28:34 +0530

    completed the add add account

--- src/bankmanager.py
+++ src/bankmanager.py
@@ -2,11 +2,13 @@ from utils.valid_input import (get_valid_amount,get_valid_name)
 from src.accounts import Account
 import random
 import string
+import json
 class Banking_system:
     filename = "data/accounts.json"
     def __init__(self,filename,accountnumber,name,pin,balance = 0):
         self.data = Account(self,filename,accountnumber,name,pin,balance)
         self.acc_holder_data = []
+    def save_data(self):
     def add_account(self):
         while True:
             #asking if user want to continue
@@ -31,13 +33,27 @@ class Banking_system:
             self.data.name = user_name
             print(f"entered name:{self.data.name}")
             #making the 4 digit pincode
-            
+            pincode = string.digits
+            for i in range(1,4):
+                self.data.__pin += random.choice(pincode)
+            print("Pin code generated request the servers to reveal the pincode\n")
+            #adding money to account
+            print("rule - account holder must deposit 500 rupees to start account")
+            while True:
+                try:
+                    Balance = int(input("deposit the money"))
+                    if Balance < 500:
+                        print("enter atleast 500 rupees").title()
+                        continue
+                    break
+                except(ValueError or TypeError):#will use gui in future to have digit only keyword to enter input
+                    print("enter digit only")
+                    continue
+            print("request the servers to know what's your balance")
+                   
 
-            
 
 
-                
-            
 
     def add_amount(self):
         while self.chances > 0:
@@ -51,7 +67,7 @@ class Banking_system:
                 amount = input("enter the amount")
                 entered_amount = get_valid_amount(amount)
                 if entered_amount is not None:
-                    self.__balance += entered_amount
+                    self.data.__balance += entered_amount
                     print(f"entered amount{entered_amount}\n".title())
                     print("updated balance ")
                     self.balance()
