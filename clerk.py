50fc0c615b57730e8da23a988a50da3eab8a5a8f
jass2k5 <jsssndl@gmail.com>
2025-09-11 01:55:16 +0530

    cleared all files from repo

--- clerk.py
+++
@@ -1,5 +0,0 @@
-from src.bankmanager import Banking_system
-from src.accounts import Account
-print("welcome to the bank of oops").title()
-filename = "data/accounts.json"
-transfer = Banking_system(filename)
